c11d3d5d54fd62f4a67a011762fe7ab7264497ed
Jainab Hadisa <jhadisa@deloitte.com>
2026-08-12 13:08:23 +0530

    Added variables datatype questions

---
+++ Variables_Datatypes/Introduction/Question1.java
@@ -0,0 +1,21 @@
+package Variables_Datatypes.Introduction;
+
+public class Question1 {
+    public static void main(String[] args) {
+        //Question
+        // Declare a variable of different data types such as int, double,
+        // char, boolean nd string, and print their values.
+        int age =24;
+        double salary = 30000.50;
+        char grade ='A';
+        boolean isEmployee = true;
+        String name = "Jainab";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Employee: " + isEmployee);
+    }
+    
+}
