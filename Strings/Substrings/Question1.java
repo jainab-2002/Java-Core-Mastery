abc5bb96b128953192a37a8a64b64d5b6d6b0e90
Jainab Hadisa <jhadisa@deloitte.com>
2026-08-14 13:20:04 +0530

    added 2 questions for substrings

---
+++ Strings/Substrings/Question1.java
@@ -0,0 +1,14 @@
+package Strings.Substrings;
+
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        String text = "Hello Java";
+
+        String result = text.substring(6);
+
+        System.out.println("Original String: " + text);
+        System.out.println("Substring: " + result);
+    }
+}
---
+++ Strings/Substrings/Question2.java
@@ -0,0 +1,12 @@
+package Strings.Substrings;
+public class Question2 {
+    public static void main(String[] args) {
+
+        String word = "Programming";
+
+        String result = word.substring(0, 7);
+
+        System.out.println("Original String: " + word);
+        System.out.println("Substring: " + result);
+    }
+}
