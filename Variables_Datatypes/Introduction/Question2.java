4c4ddfd554abf04325588f634480257c331f1701
Jainab Hadisa <jhadisa@deloitte.com>
2026-08-14 10:17:22 +0530

    Added Question2

---
+++ Variables_Datatypes/Introduction/Question2.java
@@ -0,0 +1,22 @@
+package Variables_Datatypes.Introduction;
+
+public class Question2 {
+    public static void main(String[] args) {
+        //Question
+        // Declare two integer variables and perform addition,
+        // Substraction, multiplication and division on them.
+        //print the result of each operation.
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+    
+}
