f20c68d207caefd0a3dbc1dc0c06ac9c0455e1ed
Jainab Hadisa <jhadisa@deloitte.com>
2026-08-14 14:03:23 +0530

    added 2 questions for for each loops

---
+++ Array/For_Each_Loops/Question1.java
@@ -0,0 +1,12 @@
+package Array.For_Each_Loops;
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
---
+++ Array/For_Each_Loops/Question2.java
@@ -0,0 +1,13 @@
+
+package Array.For_Each_Loops;
+
+public class Question2 {
+    public static void main(String[] args) {
+
+        int[] marks = {85, 90, 78, 95, 88};
+
+        for (int mark : marks) {
+            System.out.println("Marks: " + mark);
+        }
+    }
+}
