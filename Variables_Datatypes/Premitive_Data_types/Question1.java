619c4c03fa18a36b52054026e39e27e6728d6706
Jainab Hadisa <jhadisa@deloitte.com>
2026-08-14 10:53:17 +0530

    added 2 more questions of primitive data types

---
+++ Variables_Datatypes/Premitive_Data_types/Question1.java
@@ -0,0 +1,27 @@
+package Variables_Datatypes.Premitive_Data_types;
+
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short year = 2026;
+        int salary = 50000;
+        long population = 1400000000L;
+
+        float height = 5.8f;
+        double price = 999.99;
+
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Byte value: " + age);
+        System.out.println("Short value: " + year);
+        System.out.println("Int value: " + salary);
+        System.out.println("Long value: " + population);
+        System.out.println("Float value: " + height);
+        System.out.println("Double value: " + price);
+        System.out.println("Char value: " + grade);
+        System.out.println("Boolean value: " + passed);
+    }
+}
---
+++ Variables_Datatypes/Premitive_Data_types/Question2.java
@@ -0,0 +1,26 @@
+package Variables_Datatypes.Premitive_Data_types;
+
+public class Question2 {
+    public static void main(String[] args) {
+
+        byte age = 18;
+        short marks = 450;
+        int rollNumber = 101;
+        long phoneNumber = 9876543210L;
+
+        float percentage = 90.5f;
+        double fees = 25000.75;
+
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Fees: " + fees);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
