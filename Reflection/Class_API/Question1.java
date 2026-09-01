ce34b15c3059e3ac85d6a1c304d250d5c04f8e6c
Jainab Hadisa <jhadisa@deloitte.com>
2026-09-01 10:44:44 +0530

    added 2 question of class api

---
+++ Reflection/Class_API/Question1.java
@@ -0,0 +1,14 @@
+package Reflection.Class_API;
+
+public class Question1 {
+    public static void main(String[] args) {
+        String name = "Jainab";
+
+        System.out.println(name.length());
+
+        System.out.println(name.toUpperCase());
+              System.out.println(name.charAt(0));
+
+
+    }
+}
---
+++ Reflection/Class_API/Question2.java
@@ -0,0 +1,13 @@
+package Reflection.Class_API;
+
+public class Question2 {
+    public static void main(String[] args) {
+        int num = 25;
+
+        System.out.println(Math.sqrt(num));
+        System.out.println(Math.max(10,20));
+
+        System.out.println(Math.pow(2,3));
+}
+
+}
