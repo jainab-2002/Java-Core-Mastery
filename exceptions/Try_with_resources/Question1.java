c245205f58ced04fa15f49974915864a469ab167
Jainab Hadisa <jhadisa@deloitte.com>
2026-09-03 14:07:56 +0530

    added 2 questions fot Try_with_resources

---
+++ exceptions/Try_with_resources/Question1.java
@@ -0,0 +1,21 @@
+package exceptions.Try_with_resources;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        try (FileReader file = new FileReader("test.txt")) {
+
+            int data;
+
+            while ((data = file.read()) != -1) {
+                System.out.print((char) data);
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println("File error occurred.");
+        }
+    }
+}
